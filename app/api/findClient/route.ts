141cc44e2f2a3ceae16421872fff15f6c8010cb5
Jakub <60876248+CZARNY77@users.noreply.github.com>
2025-01-24 21:58:35 +0100

    3 - bacl

--- app/api/findClient/route.ts
+++ app/api/findClient/route.ts
@@ -2,14 +2,6 @@ import { z } from "zod";
 import findClientSchema from "./schema";
 import { NextResponse } from "next/server";
 
-function createNextResponse(body: any, status: number) {
-  const response = NextResponse.json(body, { status });
-  response.headers.set("Access-Control-Allow-Origin", "*"); // Zmień na swoją domenę w produkcji
-  response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
-  return response;
-}
-
 export async function GET(request: Request) {
   
     // Odczyt i walidacja danych wejściowych
@@ -17,7 +9,7 @@ export async function GET(request: Request) {
     const email = searchParams.get("email");
   try {
     if (!email) {
-      return createNextResponse({ error: "Email is required" }, 400);
+        return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
     findClientSchema.parse({ email });
 
@@ -31,15 +23,20 @@ export async function GET(request: Request) {
     // Jeśli znaleziono klienta, zwróć dane
     if (data.result && data.result.length > 0) {
       const client = data.result[0];
-      return createNextResponse({ id: client.ID }, 200);
+      return NextResponse.json(
+        {
+          id: client.ID,
+        },
+        { status: 200 }
+      );
     }
 
     // Jeśli klienta nie znaleziono
-    return createNextResponse({ error: "Client not found." }, 404);
+    return NextResponse.json({ error: "Client not found." }, { status: 404 });
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return createNextResponse({ error: error.message }, 400);
+      return NextResponse.json({ error: error.message }, { status: 400 });
     }
-    return createNextResponse({ error: "Internal Server Error" }, 500);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
